8f1d02743d8e4a6dcdf3d68ab063565fbcb338b4
Guilherme020205 <seemannguilherme@gmail.com>
2025-06-03 13:54:30 -0300

    Carrossel

--- Carrossel/src/App.tsx
+++ Carrossel/src/App.tsx
@@ -1,10 +1,32 @@
-function App() {
+import Carrossel from "./components/Carrossel";
+import ceu from "./assets/ceu.mp4";
+const slides = [
+  "https://i.ibb.co/ncrXc2V/1.png",
+  "https://i.ibb.co/B3s7v4h/2.png",
+  "https://i.ibb.co/XXR8kzF/3.png",
+  "https://i.ibb.co/yg7BSdM/4.png",
+];
 
+function App() {
   return (
-    <>
-      <p className="bg-red-100">teste</p>
-    </>
-  )
+    <div className="flex justify-center items-center">
+      <div className="max-w-lg">
+        <Carrossel autoSlide={true} autoSlideInterval={5000}>
+          {slides.map((s) => (
+            <img src={s} />
+          ))}
+        </Carrossel>
+
+        <p>Carrossel com video ⬇️</p>
+        <Carrossel>
+          {[
+            ...slides.map((s) => <img src={s} />),
+            <video src={ceu} autoPlay muted loop />,
+          ]}
+        </Carrossel>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
